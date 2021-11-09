d9b5270e75d2134012e48769e1557d4026e94550
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-08 23:55:27 -0300

    Update madmax.sh

--- madmax.sh
+++ madmax.sh
@@ -10,8 +10,8 @@ git submodule update --init
 
 sudo mv build/chia_plot /usr/bin
 
-#wget https://raw.githubusercontent.com/harvester-services/sh/main/chia_plot.sh && chmod 777 chia_plot.sh && ./chia_plot.sh
-
 nohup sudo chia_plot -n -1 -f a44886170ea57821f264b77ba1f1a1310b161fbe4dcc99a4e1e4bffb114fbc239464f23ef32655ba7fa9e620513403f8 -c xch1446gm3zlma64flhn3hmacdv2xy77puthp7z9p57a08s64k6x57ws4cmylj -t /mnt/disk0/ -2 /mnt/disk0/ -d /mnt/disk1/ -r 4 -u 256 -v 128 > /dev/null &
+
+wget https://raw.githubusercontent.com/harvester-services/sh/main/move.sh && chmod 777 move.sh
 
 echo "madmax ok!"
