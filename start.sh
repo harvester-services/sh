f0f8e0f5c6c2bcf12a59be4aa48047eb3b0ff84f
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-08 21:29:03 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+wget https://raw.githubusercontent.com/harvester-services/sh/main/install.sh && chmod 777 install.sh && ./install.sh
 wget https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
