1e772cd69b736d9d037f871cebcd2428becdf643
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-11 00:09:00 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/harvester-services/sh/main/install.sh && chmod 777 install.sh && ./install.sh
+mkdir /home/harvester40/teste
 
-wget https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/install.sh && chmod 777 install.sh && ./install.sh
 
-wget https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
 
-wget https://raw.githubusercontent.com/harvester-services/sh/main/madmax.sh && chmod 777 madmax.sh && ./madmax.sh
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
 
-wget https://raw.githubusercontent.com/harvester-services/sh/main/move.sh && chmod 777 move.sh && nohup ./move.sh > /dev/null &
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/madmax.sh && chmod 777 madmax.sh && ./madmax.sh
+
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/move.sh && chmod 777 move.sh && nohup ./move.sh > /dev/null &
 
 touch OK
