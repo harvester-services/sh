40bf57072ecf2218985d86882606d9ec39f52b1a
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-11 02:05:53 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir /home/harvester40/teste
-
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/install.sh && chmod 777 install.sh && ./install.sh
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
