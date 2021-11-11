4a320883e4e71693f617499ee4e7a4ac779e974a
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-10 23:38:18 -0300

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -40,7 +40,7 @@ for assinatura in "${subscription[@]}"
          nome=$(date +"%d%m%Y%H%M%S")
 
          echo "Criando VM $nome ($i) na região $regiao da Subscription $assinatura"
-         az vm create --resource-group $RG --name $nome --size "Standard_F8" --image UbuntuLTS --public-ip-sku Standard --accelerated-networking=true --authentication-type=password --admin-username=azure --admin-password=qpalzm794613Q! --data-disk-sizes-gb 512 512         
+         az vm create --location $regiao --resource-group $RG --name $nome --size "Standard_F8" --image UbuntuLTS --public-ip-sku Standard --accelerated-networking=true --authentication-type=password --admin-username=azure --admin-password=qpalzm794613Q! --data-disk-sizes-gb 512 512         
 
          CriandoVM=$(az vm list --query "[?name=='$nome'].{Nome:name}" -o tsv)     
          
