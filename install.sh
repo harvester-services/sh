7ad5dee722680ea4d1d27ed2f0fe520e03f6a82c
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-11 01:18:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt remove --purge --auto-remove cmake
+sudo apt remove -y --purge --auto-remove cmake
 
 sudo apt update && \
 sudo apt install -y software-properties-common lsb-release && \
