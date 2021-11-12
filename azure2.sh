f4bc0bf57309c3380ac58ff5a3fd4b75f4519c20
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-12 11:51:14 -0300

    Update azure2.sh

--- azure2.sh
+++ azure2.sh
@@ -49,7 +49,7 @@ for assinatura in "${subscription[@]}"
      nome=$(date +"%d%m%Y%H%M%S")
 
      echo "Criando VM $nome na região $regiao da Subscription $assinatura com user $user"
-     az vm create --location $regiao --resource-group $RG --name $nome --size "Standard_F8" --image UbuntuLTS --public-ip-sku Standard --accelerated-networking=true --authentication-type=password --admin-username=$user --admin-password=qpalzm794613Q! --data-disk-sizes-gb 512 512         
+     az vm create --location $regiao --resource-group $RG --name $nome --size "Standard_F4s_v2" --image UbuntuLTS --public-ip-sku Standard --accelerated-networking=true --authentication-type=password --admin-username=$user --admin-password=qpalzm794613Q! --data-disk-sizes-gb 512 512         
 
      echo "Criando Extension da VM $nome na região $regiao da Subscription $assinatura"
          
