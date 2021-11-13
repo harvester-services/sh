bab30f32bc21e1d8d2ec9c03728346eb330c5981
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-13 11:47:23 -0300

    Update chia_plot.sh

--- chia_plot.sh
+++ chia_plot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup sudo chia_plot -n -1 -f a44886170ea57821f264b77ba1f1a1310b161fbe4dcc99a4e1e4bffb114fbc239464f23ef32655ba7fa9e620513403f8 -c xch1446gm3zlma64flhn3hmacdv2xy77puthp7z9p57a08s64k6x57ws4cmylj -t /mnt/disk0/ -2 /mnt/disk0/ -d /mnt/disk1/ -r 4 -u 256 -v 128 > /dev/null &
+chia_plot -n -1 -f a44886170ea57821f264b77ba1f1a1310b161fbe4dcc99a4e1e4bffb114fbc239464f23ef32655ba7fa9e620513403f8 -c xch1446gm3zlma64flhn3hmacdv2xy77puthp7z9p57a08s64k6x57ws4cmylj -t /mnt/disk0/ -2 /mnt/disk0/ -d /mnt/disk1/ -r 4 -u 256 -v 128
