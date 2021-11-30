472af69c17dcbaf4a26424dea2f290c258b360ac
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-30 09:01:12 -0300

    Update move.sh

--- move.sh
+++ move.sh
@@ -25,10 +25,10 @@ do
    then
       echo
       echo "Enviando para gdrive..."
-      sudo rclone copy /mnt/disk1/ $account --progress --drive-stop-on-upload-limit --drive-stop-on-download-limit --include "*.plot" --log-level ERROR --log-file=/root/.config/rclone/gdrive.log     
+      sudo rclone copy /mnt/disk1/ $account --progress --drive-stop-on-upload-limit --drive-stop-on-download-limit --include "*.plot"     
       echo
       echo "Enviando para dropbox..."
-      sudo rclone move /mnt/disk1/ dropbox:azure/ --progress --drive-stop-on-upload-limit --drive-stop-on-download-limit --include "*.plot" --log-level ERROR --log-file=/root/.config/rclone/dropbox.log 
+      sudo rclone move /mnt/disk1/ dropbox:azure/ --progress --drive-stop-on-upload-limit --drive-stop-on-download-limit --include "*.plot" 
       echo
       echo "OK!"
       date >> OK
