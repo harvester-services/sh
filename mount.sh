d45436b44bdbfc67d3c19e4d53e6c8faff8254e6
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-11-30 10:34:48 -0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,16 +1,34 @@
+  GNU nano 5.6.1                                                                   mount.sh
 #!/bin/bash
 
 log='/root/.config/rclone/rclone.log'
+
+rm -rf $log && touch $log
+
+pkill rclone
+fusermount -q -u /mnt/farm1
+
+if ! pgrep rclone > /dev/null
+then
+   echo
+   date
+   echo 'RCLONE não está aberto'
+   source /root/next.sh
+   rclone mount $harvester: /mnt/farm1 --daemon --log-level DEBUG --log-file=/root/.config/rclone/rclone.log
+   echo 'Iniciado com '$harvester
+   echo
+fi
 
 while true
 do
 
   if grep -q "downloadQuotaExceeded" $log
   then
-     rm -rf $log
-     source /root/next.sh
+     rm -rf $log && touch $log
+     pkill rclone
      fusermount -q -u /mnt/farm1
-     rclone mount $harvester: /mnt/farm1 --log-level DEBUG --log-file=/root/.config/rclone/rclone.log
+     source /root/next.sh
+     rclone mount $harvester: /mnt/farm1 --daemon --log-level DEBUG --log-file=/root/.config/rclone/rclone.log
      date
      echo $harvester
      echo
