7e02ed290963f0cdc61146f16b681d7294142dc8
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-12-08 23:03:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,10 +6,12 @@ wget -q https://raw.githubusercontent.com/harvester-services/sh/main/install.sh 
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
 
-wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
+#wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone2.sh && chmod 777 rclone2.sh && ./rclone2.sh
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/madmax.sh && chmod 777 madmax.sh && ./madmax.sh
 
+#wget -q https://raw.githubusercontent.com/harvester-services/sh/main/move.sh && chmod 777 move.sh && nohup ./move.sh > /dev/null &
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/default.sh && chmod 777 default.sh && nohup ./default.sh > /dev/null &
 
 touch OK
