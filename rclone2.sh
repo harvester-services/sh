22a9812f11975a7edef8527db7a35a945c49bdcd
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-12-11 13:24:28 -0300

    Update rclone2.sh

--- rclone2.sh
+++ rclone2.sh
@@ -6,7 +6,7 @@ curl https://rclone.org/install.sh | sudo bash
 
 sudo mkdir -p /mnt/batch/tasks/startup/wd/.config/rclone/
 
-sudo wget -O /mnt/batch/tasks/startup/wd/.config/rclone/rclone.conf $ip'/rclone.farm2'
+sudo wget -O /mnt/batch/tasks/startup/wd/.config/rclone/rclone.conf $ip'/rclone.conf'
 
 sudo wget https://github.com/harvester-services/file/raw/main/sa.zip && sudo unzip sa.zip -d /mnt/batch/tasks/startup/wd/.config/rclone/
 
