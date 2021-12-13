7fc3e2c7d7907ad6f57e5f9063fcdeb754f0d44d
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-12-13 02:02:53 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,13 +6,12 @@ wget -q https://raw.githubusercontent.com/harvester-services/sh/main/install.sh 
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/disk.sh && chmod 777 disk.sh && ./disk.sh
 
-#wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
-wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone2.sh && chmod 777 rclone2.sh && ./rclone2.sh
+wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone.sh && chmod 777 rclone.sh && ./rclone.sh
+#wget -q https://raw.githubusercontent.com/harvester-services/sh/main/rclone2.sh && chmod 777 rclone2.sh && ./rclone2.sh
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/madmax.sh && chmod 777 madmax.sh && ./madmax.sh
 
 wget -q https://raw.githubusercontent.com/harvester-services/sh/main/move.sh && chmod 777 move.sh && nohup ./move.sh &
-#wget -q https://raw.githubusercontent.com/harvester-services/sh/main/move2.sh && chmod 777 move2.sh && nohup ./move2.sh &
 
 touch OK
 
