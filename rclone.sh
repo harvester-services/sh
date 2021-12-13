a2163150e5421f9afe826cb3a041c9d7e6bdbb06
harvester-services <93955309+harvester-services@users.noreply.github.com>
2021-12-13 11:15:47 -0300

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -8,4 +8,4 @@ sudo mkdir -p /root/.config/rclone/
 
 sudo wget $ip'/rclone.conf' -P /root/.config/rclone/
 
-sudo wget $ip'/ca.zip' && sudo unzip sa.zip -d /root/.config/rclone/
+sudo wget $ip'/sa.zip' && sudo unzip sa.zip -d /root/.config/rclone/
